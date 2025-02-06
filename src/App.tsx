074b9940cf66c2fbb01fe1aae401adfe607cead1
gpuc93 <48922577+gpuc93@users.noreply.github.com>
2025-02-06 10:00:44 -0600

    Add invoice route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css'
 import InvoiceList from './pages/invoiceList';
+import InvoiceForm from "./pages/invoiceForm";
 
 function App() {
   return (
     <>
     <div>
+
       <Routes>
         <Route path="/" element={<InvoiceList />} />
+          <Route path="/create" element={<InvoiceForm />} />
+          {/*
+          <Route path="/edit/:id" element={<InvoiceForm editMode />} />*/}
         </Routes>
     </div>
+      
     </>
   )
 }
---
+++ src/pages/invoiceForm.tsx
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+import '../assets/css/newInvoice.css'
+
+interface InvoiceFormProps {
+    editMode?: boolean
+}
+
+export default function InvoiceForm ({ editMode }: InvoiceFormProps) {
+
+    return (
+      <h1>InvoiceForm</h1>
+    )
+}
